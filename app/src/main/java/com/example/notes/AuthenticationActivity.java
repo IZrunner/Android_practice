a415b482dd66f91a586b5277a42f12aa999e5e84
IZrunner <masterchefchinatown@gmail.com>
2019-10-27 17:50:20 +0200

    minor bug fixes
    auth activity launches on app start, user is verified. new bug : to go to main activity, the 'sign in'-button has to be pressed twice (1 - to sign in, 2 - to switch activity)

--- app/src/main/java/com/example/notes/AuthenticationActivity.java
+++ app/src/main/java/com/example/notes/AuthenticationActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,39 +17,61 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class AuthenticationActivity extends AppCompatActivity {
     private Button mSignInButton, mSignOutButton;
-    private String mEmail, mPassword;
+    private EditText mEmail, mPassword;
     private static final String TAG = "AuthenticationActivity";
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
 
-        mAuth = FirebaseAuth.getInstance();
-
         mSignInButton = findViewById(R.id.signInButton);
         mSignOutButton = findViewById(R.id.signOutButton);
-        mEmail = findViewById(R.id.emailText).toString();
-        mPassword = findViewById(R.id.passwordText).toString();
+        mEmail = findViewById(R.id.emailText);
+        mPassword = findViewById(R.id.passwordText);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Toast.makeText(AuthenticationActivity.this, "Successfully signed in with: " + user.getEmail(),
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Toast.makeText(AuthenticationActivity.this, "Successfully signed out.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
 
         mSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!mEmail.equals("") && !mPassword.equals("")) {
-                    mAuth.signInWithEmailAndPassword(mEmail, mPassword);
+                String email = mEmail.getText().toString();
+                String pass = mPassword.getText().toString();
+                if (!email.equals("") && !pass.equals("")) {
+                    mAuth.signInWithEmailAndPassword(email, pass);
                     FirebaseUser user = mAuth.getCurrentUser();
                     if (user != null) {
-                        updateUI(user);
+                        Intent k = new Intent(AuthenticationActivity.this, MainActivity.class);
+                        startActivity(k);
                         Toast.makeText(AuthenticationActivity.this, "User verified, " + user.getUid(),
                                 Toast.LENGTH_SHORT).show();
-                        finish();
+                        //finish();
                     }
                 }
                 else {
                     Toast.makeText(AuthenticationActivity.this, "Authentication failed.",
                             Toast.LENGTH_SHORT).show();
-                    updateUI(null);
+                    //updateUI(null);
                 }
             }
         });
@@ -63,12 +87,23 @@ public class AuthenticationActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-        Toast.makeText(AuthenticationActivity.this, "User is already logged in, " + currentUser.getUid(),
-                Toast.LENGTH_SHORT).show();
-        finish();
+        if (currentUser != null) {
+            Toast.makeText(AuthenticationActivity.this, "User is already logged in, " + currentUser.getUid(),
+                    Toast.LENGTH_SHORT).show();
+            updateUI(currentUser);
+        }
+        //finish();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     private void updateUI(FirebaseUser user) {
